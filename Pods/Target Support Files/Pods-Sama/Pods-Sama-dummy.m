6d6a4c9c2f6ae4c69dbecbc5233bea71f8e1630b
Andre Suhartanto <andresuhartanto28@gmail.com>
2019-09-20 12:39:59 +0800

    First commit

---
+++ Pods/Target Support Files/Pods-Sama/Pods-Sama-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sama : NSObject
+@end
+@implementation PodsDummy_Pods_Sama
+@end
---
+++ Sama/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Sama
+//
+//  Created by Andre Suhartanto on 19/09/2019.
+//  Copyright © 2019 Andre Suhartanto. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
