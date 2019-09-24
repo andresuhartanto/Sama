d46b24a63960cbde9219563985055c5b80f7eb57
Andre Suhartanto <andresuhartanto28@gmail.com>
2019-09-24 17:15:33 +0800

    Add tableview Header

--- Sama/Controller/MainScreenViewController.swift
+++ Sama/Controller/MainScreenViewController.swift
@@ -13,6 +13,7 @@ class MainScreenViewController: UIViewController, UITableViewDataSource, UITable
     var testData = ["Water Bill", "Netflix", "Groceries", "Internet", "Maintenace", "Cleaning Lady", "Cat food"]
     
     @IBOutlet weak var itemTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,10 +23,38 @@ class MainScreenViewController: UIViewController, UITableViewDataSource, UITable
         
         // Register CustomItemCell.xib
         itemTableView.register(UINib(nibName: "CustomItemCell", bundle: nil), forCellReuseIdentifier: "customItemCell")
+        
+        // Register CustomHeader.xib
+        let nib = UINib(nibName: "CustomHeader", bundle: nil)
+        itemTableView.register(nib, forHeaderFooterViewReuseIdentifier: "CustomHeader")
+        
+        let statusBarView = UIView(frame: UIApplication.shared.statusBarFrame)
+        let statusBarColor = UIColor(red: 60/255, green: 193/255, blue: 246/255, alpha: 1.0)
+        statusBarView.backgroundColor = statusBarColor
+        view.addSubview(statusBarView)
 
-        // Do any additional setup after loading the view.
     }
     
+    // Header Creation
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "CustomHeader") as! CustomHeader
+        header.pocketBtn.setTitle("Home", for: .normal)
+        header.firstUserNameLabel.text = "Andre"
+        header.secondUserNameLabel.text = "Jerome"
+        header.totalLabel.text = "$1.980"
+        
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Cell Creation
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customItemCell", for: indexPath) as! CustomItemCell
         
---
+++ Sama/View/Custom Header/CustomHeader.swift
@@ -0,0 +1,19 @@
+//
+//  CustomHeader.swift
+//  Sama
+//
+//  Created by Andre Suhartanto on 24/09/2019.
+//  Copyright © 2019 Andre Suhartanto. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeader: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var pocketBtn: UIButton!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var firstUserNameLabel: UILabel!
+    @IBOutlet weak var secondUserNameLabel: UILabel!
+    
+
+}
