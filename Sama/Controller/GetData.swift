829edacef9ffa6dbe7aff417e275882fb56bb142
Andre Suhartanto <andresuhartanto28@gmail.com>
2019-10-09 10:34:47 +0800

    Fix load item bugs where if pocket is empty it wont load pocket detail

--- Sama/Controller/GetData.swift
+++ Sama/Controller/GetData.swift
@@ -13,29 +13,31 @@ var userUID = getuserID()
 
 func getActivePocket(completion: @escaping (Pocket) -> Void) {
     let ref = Database.database().reference()
-    let pocket = Pocket()
     ref.child("Users").child(userUID).child("activePocket").observe(.childAdded) { (snapshot) in
         let pocketRef = ref.child("Pockets").child(snapshot.key)
         pocketRef.observeSingleEvent(of: .value) { (pocketSnapshot) in
             let snapshotValue = pocketSnapshot.value as! Dictionary<String, Any>
-
+            
+            let pocket = Pocket()
             let pocketID = pocketSnapshot.key
             let pocketName = snapshotValue["name"]
             let pocketContributors = snapshotValue["contributors"] as! Dictionary<String, Any>
             
+            pocket.pocketID = pocketID
+            pocket.name = pocketName as! String
+            pocket.contributors = pocketContributors
+            
             if let items = snapshotValue["items"] as? [String : Bool] {
                 loadData(items, ref) { (itemArray) in
-                    pocket.pocketID = pocketID
-                    pocket.name = pocketName as! String
-                    pocket.contributors = pocketContributors
+                    
                     pocket.items = itemArray
                     
                     completion(pocket)
                 }
             }
+            completion(pocket)
         }
     }
-    completion(pocket)
 }
 
 func loadData(_ items : [String : Bool], _ ref : DatabaseReference, completion: @escaping ([Item]) -> Void) {
